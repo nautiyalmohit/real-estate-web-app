fbe98f47df19836ec3ec701e6ea22c0016c9f8b0
nautiyalmohit <mohit8nautiyal@gmail.com>
2023-06-24 00:19:22 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header/Header';
+import './index.css'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Hero.css';
+const Hero = () => {
+    return (
+        <section className="hero-wrapper">
+            <div className=" flexCenter paddings innerWidth hero-containe">
+
+            </div>
+
+        </section>
+    )
+}
+export default Hero;
